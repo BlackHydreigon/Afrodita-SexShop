0f03bb5fb0d8b709621480d10d89d77f88a880b7
BlackHydreigon <juahgghgj@gmail.com>
2024-12-06 03:14:52 -0500

    Primer repositorio

---
+++ includes/footer.php
@@ -0,0 +1,117 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Afrodita SexShop JA</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us">El lugar donde comprar tus fantasias</p>
+    
+        <div class="social-icons">
+            
+        <a href="https://www.facebook.com/profile.php?id=100066988202178" class='active' target="_blank"><i class="icon fa fa-facebook"></i></a>
+        <!--<a href="#"><i class="icon fa fa-twitter"></i></a>-->
+        <a href="https://www.instagram.com/afroditajasexshop/"  target="_blank"><i class="icon fa fa-instagram"></i></a>
+
+        </div><!-- /.social-icons -->
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT TIMING============================================================= -->
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">Horarios de atencion</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Lunes:</td><td class="pull-right">10:00 a.m - 8:00 p.m</td></tr>
+					<tr><td>Martes:</td><td class="pull-right">10:00 a.m - 8:00 p.m</td></tr>
+					<tr><td>Miercoles:</td><td class="pull-right">10:00 a.m - 8:00 p.m</td></tr>
+                    <tr><td>Jueves:</td><td class="pull-right">10:00 a.m - 8:00 p.m</td></tr>
+					<tr><td>Viernes:</td><td class="pull-right">10:00 a.m - 9:00 p.m</td></tr>
+					<tr><td>Sabado:</td><td class="pull-right">10:00 a.m - 9:00 p.m</td></tr>
+					<tr><td>Domingo:</td><td class="pull-right">11:00 a.m - 7:00 p.m</td></tr>
+
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">Informacion</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <a href="https://maps.app.goo.gl/fsBcYoRbAbM8sYVY7" target="_blank"><p>Cra.47#53-42,Las Granias,Bello,Antioquia</p></a>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>+57 3127494925</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="#">afroditajasexshop@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            
+</div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+    <div class="container">
+			
+			<b class="copyright">&copy; 2024 Afrodita J&A SexShop </b> Todos los derechos reservados.
+		</div>
+
+</footer>
